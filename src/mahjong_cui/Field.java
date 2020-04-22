39f97f2e259dc7514183cb93f689b4e930112e05
Snowman-s <31811@toyota.kosen-ac.jp>
2020-04-22 13:07:38 +0900

    Field定義

---
+++ src/mahjong_cui/Field.java
@@ -0,0 +1,12 @@
+package mahjong_cui;
+
+import mahjong_cui.player.Player;
+import mahjong_cui.tile.Tile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Field {
+    private List<Tile> shonPai = new ArrayList<>();
+    private List<Player> playerList = new ArrayList<>();
+}
