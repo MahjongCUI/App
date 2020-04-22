deda44649427c58f7e89f22e45022b45d85966ff
Snowman-s <31811@toyota.kosen-ac.jp>
2020-04-22 13:05:33 +0900

    Player定義

---
+++ src/mahjong_cui/player/Player.java
@@ -0,0 +1,11 @@
+package mahjong_cui.player;
+
+import mahjong_cui.tile.Tile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+    private List<Tile> shoPai = new ArrayList<>();
+    private List<Tile> sutePai = new ArrayList<>();
+}
