7143ed08d99ae7df3be5acc55f599bf7af9c7067
Snowman-s <31811@toyota.kosen-ac.jp>
2020-04-22 15:24:49 +0900

    Fieldのdeckを表示できるようにした

--- src/mahjong_cui/Field.java
+++ src/mahjong_cui/Field.java
@@ -3,12 +3,24 @@ package mahjong_cui;
 import mahjong_cui.player.Player;
 import mahjong_cui.tile.Tile;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Field {
-    private List<Tile> deck = new ArrayList<>(136);
-    private List<Player> playerList = new ArrayList<>();
+    private List<Tile> deck;
+    private List<Player> playerList;
+
+    public Field(List<Tile> deck, List<Player> players) {
+        this.deck = deck;
+        this.shuffle();
+        this.playerList = players;
+    }
+
+    /**
+     * deckをシャッフルします。
+     */
+    public void shuffle() {
+
+    }
 
     public List<Tile> getDeck() {
         return deck;
--- src/mahjong_cui/Main.java
+++ src/mahjong_cui/Main.java
@@ -1,9 +1,14 @@
 package mahjong_cui;
 
+import mahjong_cui.tile.Tile;
+
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-        Field field = new Field();
-	// write your code here
+        Field field = new Field(Tile.tileSet(), new ArrayList<>());
+
+        System.out.println("field.getDeck() = " + field.getDeck().toString().replace("},", "},\n"));
     }
 }
