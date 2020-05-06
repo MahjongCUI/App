926b2c7ece8e5865be109ada720278f618925fa7
Snowman-s <31811@toyota.kosen-ac.jp>
2020-05-06 14:28:16 +0900

    インスタンス化禁止

--- src/mahjong_cui/tile/RollOverJudge.java
+++ src/mahjong_cui/tile/RollOverJudge.java
@@ -2,7 +2,12 @@ package mahjong_cui.tile;
 
 import mahjong_cui.player.Player;
 
-public class RollOverJudge {
+public final class RollOverJudge {
+    //インスタンス化禁止
+    private RollOverJudge(){
+
+    }
+
     public boolean isRollOver(Player player) {
         return false;
     }
