ba8303b93085b415bf5c3ec27186deef3e982a46
Snowman-s <31811@toyota.kosen-ac.jp>
2020-05-16 23:43:51 +0900

    TileTypeについてコンストラクタで決定するように変更

--- src/mahjong_cui/tile/TileType.java
+++ src/mahjong_cui/tile/TileType.java
@@ -1,51 +1,94 @@
 package mahjong_cui.tile;
 
 public enum TileType {
-    Sozu,
-    Pinzu,
-    Manzu,
-    East,
-    South,
-    West,
-    North,
-    Haku,
-    Hatsu,
-    Chun,
+    Sozu(TileTypeType.NUMBER, "索"),
+    Pinzu(TileTypeType.NUMBER, "筒"),
+    Manzu(TileTypeType.NUMBER, "萬"),
+    East(TileTypeType.WIND, "西"),
+    South(TileTypeType.WIND, "南"),
+    West(TileTypeType.WIND, "東"),
+    North(TileTypeType.WIND, "北"),
+    Haku(TileTypeType.DRAGON, "白"),
+    Hatsu(TileTypeType.DRAGON, "撥"),
+    Chun(TileTypeType.DRAGON, "中"),
     ;
 
+    private final TileTypeType tileTypeType;
+    private final String kanji;
+
+    TileType(TileTypeType tileTypeType, String kanji) {
+        this.tileTypeType = tileTypeType;
+        this.kanji = kanji;
+    }
+
     public boolean isNumTile() {
-        return switch (this) {
-            case Sozu, Pinzu, Manzu -> true;
-            default -> false;
-        };
+        return tileTypeType.isNumTile();
     }
 
     public boolean isDragonTile() {
-        return switch (this) {
-            case Haku, Hatsu, Chun -> true;
-            default -> false;
-        };
+        return tileTypeType.isDragonTile();
     }
 
     public boolean isWindTile() {
-        return switch (this) {
-            case East, South, West, North -> true;
-            default -> false;
-        };
+        return tileTypeType.isWindTile();
     }
 
     public String getKanjiValue() {
-        return switch (this) {
-            case Sozu -> "索";
-            case Pinzu -> "筒";
-            case Manzu -> "萬";
-            case East -> "東";
-            case West -> "西";
-            case North -> "北";
-            case South -> "南";
-            case Haku -> "白";
-            case Hatsu -> "發";
-            case Chun -> "中";
+        return kanji;
+    }
+
+    private enum TileTypeType {
+        NUMBER {
+            @Override
+            public boolean isNumTile() {
+                return true;
+            }
+
+            @Override
+            public boolean isDragonTile() {
+                return false;
+            }
+
+            @Override
+            public boolean isWindTile() {
+                return false;
+            }
+        }, DRAGON {
+            @Override
+            public boolean isNumTile() {
+                return false;
+            }
+
+            @Override
+            public boolean isDragonTile() {
+                return true;
+            }
+
+            @Override
+            public boolean isWindTile() {
+                return false;
+            }
+        }, WIND {
+            @Override
+            public boolean isNumTile() {
+                return false;
+            }
+
+            @Override
+            public boolean isDragonTile() {
+                return false;
+            }
+
+            @Override
+            public boolean isWindTile() {
+                return true;
+            }
         };
+
+        public abstract boolean isNumTile();
+
+        public abstract boolean isDragonTile();
+
+        public abstract boolean isWindTile();
     }
 }
